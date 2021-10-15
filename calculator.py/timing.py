9ddc91c5568e3ce17c016c80790320ee81a8edd8
Rogelio Ibarra <rogelio.ibarra@sjsu.edu>
2021-10-14 18:31:37 -0700

    3rd commit

--- calculator.py/timing.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def calculate_time(func):
-	def time_it():
-		current = time.time()
-		func()
-		return current - time.time()
-	print("Total time " + str(time_it))
-
--- calculator.py/triple.py
+++
@@ -1,7 +0,0 @@
-def tripler(func):
-	def three(*args, **kwargs):
-		func(*args, **kwargs)
-		func(*args, **kwargs)
-		func(*args, **kwargs)
-		return
-	return three
