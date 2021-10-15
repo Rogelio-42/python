cdb57f53d63e207e07b22948deb598d1cf108409
Rogelio Ibarra <rogelio.ibarra@sjsu.edu>
2021-10-14 18:39:34 -0700

    6th commit

--- calculator.py
+++ calculator.py
@@ -15,8 +15,10 @@ def calculator(number1, number2, operator):
 	if operator == '*':
 		return number1 * number2
 	if operator == '/':
+		if number2 == 0:
+			return "Invalid Division"
 		return number1 / number2
 	if operator == '**':
 		return number1 ** number2
 	else:
-		return "Invalid Use"
+		return False
--- timing.py
+++ timing.py
@@ -1,9 +1,9 @@
 import time
 
 def calculate_time(func):
-	def time_it():
+	def time_it(*args, **kwargs):
 		current = time.time()
-		func()
+		func(*args, **kwargs)
 		return current - time.time()
 	print("Total time " + str(time_it))
 
